80d6562c7a203acc36f0c692deaf347d1411f6a0
xianjimli <xianjimli@gmail.com>
2018-08-19 12:26:08 +0800

    init

---
+++ src/resource.c
@@ -0,0 +1,106 @@
+#include "awtk.h"
+#include "base/resource_manager.h"
+#ifndef WITH_FS_RES
+#include "res/inc/strings/en_US.data"
+#include "res/inc/strings/zh_CN.data"
+#include "res/inc/styles/default.data"
+#include "res/inc/styles/main.data"
+#include "res/inc/ui/main.data"
+#include "res/inc/ui/window1.data"
+#ifdef WITH_STB_IMAGE
+#include "res/inc/images/arrow_down_n.res"
+#include "res/inc/images/arrow_down_o.res"
+#include "res/inc/images/arrow_down_p.res"
+#include "res/inc/images/arrow_left_n.res"
+#include "res/inc/images/arrow_left_o.res"
+#include "res/inc/images/arrow_left_p.res"
+#include "res/inc/images/arrow_right_n.res"
+#include "res/inc/images/arrow_right_o.res"
+#include "res/inc/images/arrow_right_p.res"
+#include "res/inc/images/arrow_up_n.res"
+#include "res/inc/images/arrow_up_o.res"
+#include "res/inc/images/arrow_up_p.res"
+#include "res/inc/images/check.res"
+#include "res/inc/images/checked.res"
+#include "res/inc/images/earth.res"
+#include "res/inc/images/edit_clear_n.res"
+#include "res/inc/images/edit_clear_o.res"
+#include "res/inc/images/edit_clear_p.res"
+#include "res/inc/images/empty.res"
+#include "res/inc/images/radio_checked.res"
+#include "res/inc/images/radio_unchecked.res"
+#include "res/inc/images/unchecked.res"
+#else
+#include "res/inc/images/arrow_down_n.data"
+#include "res/inc/images/arrow_down_o.data"
+#include "res/inc/images/arrow_down_p.data"
+#include "res/inc/images/arrow_left_n.data"
+#include "res/inc/images/arrow_left_o.data"
+#include "res/inc/images/arrow_left_p.data"
+#include "res/inc/images/arrow_right_n.data"
+#include "res/inc/images/arrow_right_o.data"
+#include "res/inc/images/arrow_right_p.data"
+#include "res/inc/images/arrow_up_n.data"
+#include "res/inc/images/arrow_up_o.data"
+#include "res/inc/images/arrow_up_p.data"
+#include "res/inc/images/check.data"
+#include "res/inc/images/checked.data"
+#include "res/inc/images/earth.data"
+#include "res/inc/images/edit_clear_n.data"
+#include "res/inc/images/edit_clear_o.data"
+#include "res/inc/images/edit_clear_p.data"
+#include "res/inc/images/empty.data"
+#include "res/inc/images/radio_checked.data"
+#include "res/inc/images/radio_unchecked.data"
+#include "res/inc/images/unchecked.data"
+#endif/*WITH_STB_IMAGE*/
+#ifdef WITH_STB_FONT
+#ifdef WITH_MINI_FONT
+#include "res/inc/fonts/default.mini.res"
+#else/*WITH_MINI_FONT*/
+#include "res/inc/fonts/default.res"
+#endif/*WITH_MINI_FONT*/
+#else/*WITH_STB_FONT*/
+#endif/*WITH_STB_FONT*/
+#endif/*WITH_FS_RES*/
+
+ret_t resource_init(void) {
+  resource_manager_t* rm = resource_manager();
+
+#ifdef WITH_FS_RES
+  resource_manager_load(rm, RESOURCE_TYPE_THEME, "default");
+  resource_manager_load(rm, RESOURCE_TYPE_FONT, "default");
+#else
+  resource_manager_add(rm, image_arrow_down_n);
+  resource_manager_add(rm, image_arrow_down_o);
+  resource_manager_add(rm, image_arrow_down_p);
+  resource_manager_add(rm, image_arrow_left_n);
+  resource_manager_add(rm, image_arrow_left_o);
+  resource_manager_add(rm, image_arrow_left_p);
+  resource_manager_add(rm, image_arrow_right_n);
+  resource_manager_add(rm, image_arrow_right_o);
+  resource_manager_add(rm, image_arrow_right_p);
+  resource_manager_add(rm, image_arrow_up_n);
+  resource_manager_add(rm, image_arrow_up_o);
+  resource_manager_add(rm, image_arrow_up_p);
+  resource_manager_add(rm, image_check);
+  resource_manager_add(rm, image_checked);
+  resource_manager_add(rm, image_earth);
+  resource_manager_add(rm, image_edit_clear_n);
+  resource_manager_add(rm, image_edit_clear_o);
+  resource_manager_add(rm, image_edit_clear_p);
+  resource_manager_add(rm, image_empty);
+  resource_manager_add(rm, image_radio_checked);
+  resource_manager_add(rm, image_radio_unchecked);
+  resource_manager_add(rm, image_unchecked);
+  resource_manager_add(rm, strings_en_US);
+  resource_manager_add(rm, strings_zh_CN);
+  resource_manager_add(rm, theme_default);
+  resource_manager_add(rm, theme_main);
+  resource_manager_add(rm, ui_main);
+  resource_manager_add(rm, ui_window1);
+#endif
+
+  tk_init_resources();
+  return RET_OK;
+}
---
+++ update_res.py
@@ -0,0 +1,224 @@
+#!/usr/bin/python
+
+import os
+import glob
+import shutil
+import platform
+import copy
+
+def joinPath(root, subdir):
+  return os.path.normpath(os.path.join(root, subdir))
+
+DPI='x2'
+CWD=os.getcwd()
+APP_DIR=os.getcwd()
+AWTK_ROOT=joinPath(CWD, '../awtk');
+BIN_DIR=joinPath(AWTK_ROOT, 'bin')
+INPUT_DIR=joinPath(APP_DIR, 'res/raw')
+OUTPUT_DIR=joinPath(APP_DIR, 'res/inc')
+RESOURCE_C=joinPath(APP_DIR, 'src/resource.c')
+
+OS_NAME=platform.system()
+
+def toExe(name):
+  if OS_NAME == 'Windows':
+    return joinPath(BIN_DIR, name+'.exe')
+  else:
+    return joinPath(BIN_DIR, name)
+	
+def buildAll():
+  os.system('scons')
+
+def removeDir(path):
+  if os.path.isdir(path):
+     print('rmdir:' + path);
+     shutil.rmtree(path)
+
+def prepare():
+  removeDir(OUTPUT_DIR)
+  os.makedirs(joinPath(OUTPUT_DIR, 'styles'));
+  os.makedirs(joinPath(OUTPUT_DIR, 'images'));
+  os.makedirs(joinPath(OUTPUT_DIR, 'fonts'));
+  os.makedirs(joinPath(OUTPUT_DIR, 'strings'));
+  os.makedirs(joinPath(OUTPUT_DIR, 'ui'));
+
+def themegen(raw, inc):
+  print(toExe('themegen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(OUTPUT_DIR, inc))
+  os.system(toExe('themegen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(OUTPUT_DIR, inc))
+
+def themegen_bin(raw, bin):
+  print(toExe('themegen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(INPUT_DIR, bin) + ' bin')
+  os.system(toExe('themegen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(INPUT_DIR, bin) + ' bin')
+
+def strgen(raw, inc):
+  print(toExe('strgen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(OUTPUT_DIR, inc))
+  os.system(toExe('strgen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(OUTPUT_DIR, inc))
+
+def strgen_bin(raw, bin):
+  print(toExe('strgen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(INPUT_DIR, bin) + ' bin')
+  os.system(toExe('strgen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(INPUT_DIR, bin) + ' bin')
+
+def resgen(raw, inc):
+  print(joinPath(INPUT_DIR, raw) + ' => ' + joinPath(OUTPUT_DIR, inc))
+  os.system(toExe('resgen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(OUTPUT_DIR, inc))
+
+def fontgen(raw, text, inc, size):
+  os.system(toExe('fontgen') + ' ' + joinPath(INPUT_DIR, raw) + ' ' + joinPath(INPUT_DIR, text) +' ' + joinPath(OUTPUT_DIR, inc) + ' ' + str(size))
+
+def imagegen(raw, inc):
+  print(toExe('imagegen') + ' ' + raw + ' ' + inc)
+  os.system(toExe('imagegen') + ' ' + raw + ' ' + inc)
+  inc=inc.replace('.data', '.res')
+  resgen(raw, inc)
+
+def xml_to_ui(raw, inc):
+  os.system(toExe('xml_to_ui') + ' ' + raw + ' ' + inc)
+
+def xml_to_ui_bin(raw, bin):
+  os.system(toExe('xml_to_ui') + ' ' + raw + ' ' + bin + ' bin')
+
+def gen_all_style():
+  for f in glob.glob(joinPath(INPUT_DIR, 'styles/*.xml')):
+    inc=copy.copy(f);
+    raw=copy.copy(f);
+    bin=copy.copy(f);
+    inc=inc.replace('.xml', '.data')
+    inc=inc.replace(INPUT_DIR, OUTPUT_DIR)
+    themegen(raw, inc)
+    bin=bin.replace('.xml', '.bin')
+    themegen_bin(raw, bin)
+
+
+def gen_all_image(): 
+  for f in glob.glob(joinPath(INPUT_DIR, 'images/'+DPI+'/*.*')):
+    inc=copy.copy(f);
+    raw=copy.copy(f);
+    basename=os.path.basename(inc);
+    inc=joinPath(OUTPUT_DIR, 'images/'+basename);
+    inc=inc.replace('.png', '.data')
+    inc=inc.replace('.jpg', '.data')
+    imagegen(raw, inc)
+
+def gen_all_ui():
+  for f in glob.glob(joinPath(INPUT_DIR, 'ui/*.xml')):
+    inc=copy.copy(f);
+    raw=copy.copy(f);
+    bin=copy.copy(f);
+    inc=inc.replace('.xml', '.data')
+    inc=inc.replace(INPUT_DIR, OUTPUT_DIR)
+    xml_to_ui(raw, inc)
+    bin=bin.replace('.xml', '.bin')
+    xml_to_ui_bin(raw, bin)
+
+def gen_all_font():
+  for f in glob.glob(joinPath(INPUT_DIR, 'fonts/*.ttf')):
+    res=copy.copy(f);
+    raw=copy.copy(f);
+    res=res.replace(INPUT_DIR, '.')
+    res=res.replace('.ttf', '.res');
+    raw=raw.replace(INPUT_DIR, '.')
+    resgen(raw, res)
+  fontgen('fonts/default.ttf', 'fonts/text.txt', 'fonts/default.data', 18);
+
+def gen_all_string():
+  strgen('strings/strings.xml', 'strings');
+  strgen_bin('strings/strings.xml', 'strings');
+
+def gen_gpinyin():
+  os.system(toExe('resgen') + ' ' +joinPath('3rd', 'gpinyin/data/gpinyin.dat') + ' ' + joinPath('3rd', 'gpinyin/src/gpinyin.inc'))
+  os.system(toExe('resgen') + ' ' +joinPath('tools', 'word_gen/words.bin') + ' ' + joinPath('src', 'input_methods/suggest_words.inc'))
+  os.system(toExe('resgen') + ' ' +joinPath('tools', 'word_gen/words.bin') + ' ' + joinPath('tests', 'suggest_test.inc'))
+
+def gen_all():
+  gen_all_string()
+  gen_all_font()
+  gen_all_image()
+  gen_all_ui()
+  gen_all_style()
+
+def writeResult(str):
+  fd = os.open(RESOURCE_C, os.O_RDWR|os.O_CREAT|os.O_TRUNC)
+  os.write(fd, str)
+  os.close(fd)
+
+def genIncludes(files):
+  str1 = ""
+  for f in files:
+    incf = copy.copy(f);
+    incf=incf.replace(APP_DIR, ".");
+    incf=incf.replace('\\', '/');
+    incf=incf.replace('./', '');
+    str1 += '#include "'+incf+'"\n'
+
+  return str1
+
+def gen_res_c():
+  result = '#include "awtk.h"\n'
+  result += '#include "base/resource_manager.h"\n'
+
+  result += '#ifndef WITH_FS_RES\n'
+  files=glob.glob(joinPath(OUTPUT_DIR, 'strings/*.data')) \
+    + glob.glob(joinPath(OUTPUT_DIR, 'styles/*.data')) \
+    + glob.glob(joinPath(OUTPUT_DIR, 'ui/*.data')) 
+
+  result += genIncludes(files);
+
+  result += "#ifdef WITH_STB_IMAGE\n"
+  files=glob.glob(joinPath(OUTPUT_DIR, 'images/*.res')) 
+  result += genIncludes(files)
+  result += "#else\n"
+  files=glob.glob(joinPath(OUTPUT_DIR, 'images/*.data')) 
+  result += genIncludes(files)
+  result += '#endif/*WITH_STB_IMAGE*/\n'
+
+  result += "#ifdef WITH_STB_FONT\n"
+  result += "#ifdef WITH_MINI_FONT\n"
+  files=glob.glob(joinPath(OUTPUT_DIR, 'fonts/default.mini.res')) 
+  result += genIncludes(files)
+  result += "#else/*WITH_MINI_FONT*/\n"
+  files=glob.glob(joinPath(OUTPUT_DIR, 'fonts/default.res')) 
+  result += genIncludes(files)
+  result += '#endif/*WITH_MINI_FONT*/\n'
+  result += "#else/*WITH_STB_FONT*/\n"
+  files=glob.glob(joinPath(OUTPUT_DIR, 'fonts/*.data')) 
+  result += genIncludes(files)
+  result += '#endif/*WITH_STB_FONT*/\n'
+
+  result += '#endif/*WITH_FS_RES*/\n'
+
+  result += '\n';
+  result += 'ret_t resource_init(void) {\n'
+  result += '  resource_manager_t* rm = resource_manager();\n\n'
+  result += ''
+
+  result += '#ifdef WITH_FS_RES\n'
+  result += '  resource_manager_load(rm, RESOURCE_TYPE_THEME, "default");\n'
+  result += '  resource_manager_load(rm, RESOURCE_TYPE_FONT, "default");\n'
+  result += '#else\n'
+
+  files=glob.glob(joinPath(OUTPUT_DIR, '**/*.data'))
+  for f in files:
+    incf = copy.copy(f);
+    basename = incf.replace(OUTPUT_DIR, '.');
+    basename = basename.replace('\\', '/');
+    basename = basename.replace('/fonts/', '/font/');
+    basename = basename.replace('/images/', '/image/');
+    basename = basename.replace('/styles/', '/theme/');
+    basename = basename.replace('./', '');
+    basename = basename.replace('/', '_');
+    basename = basename.replace('.data', '');
+    result += '  resource_manager_add(rm, '+basename+');\n'
+  result += '#endif\n'
+
+  result += '\n'
+  result += '  tk_init_resources();\n'
+  result += '  return RET_OK;\n'
+  result += '}\n'
+  writeResult(result);
+
+def run():
+  prepare()
+  gen_all()
+  gen_res_c()
+
+run()
