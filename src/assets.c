740d5a2e59e9a20a94a999e9e7d59f95d23ec0c4
lixianjing <xianjimli@hotmail.com>
2019-09-18 16:06:57 +0800

    update assets

--- src/assets.c
+++ src/assets.c
@@ -30,6 +30,7 @@
 #include "assets/inc/images/arrow_up_n.res"
 #include "assets/inc/images/arrow_up_o.res"
 #include "assets/inc/images/arrow_up_p.res"
+#include "assets/inc/images/backspace.res"
 #include "assets/inc/images/check.res"
 #include "assets/inc/images/checked.res"
 #include "assets/inc/images/dialog_title.res"
@@ -40,6 +41,8 @@
 #include "assets/inc/images/empty.res"
 #include "assets/inc/images/radio_checked.res"
 #include "assets/inc/images/radio_unchecked.res"
+#include "assets/inc/images/shift.res"
+#include "assets/inc/images/shifton.res"
 #include "assets/inc/images/switch.res"
 #include "assets/inc/images/unchecked.res"
 #else
@@ -55,6 +58,7 @@
 #include "assets/inc/images/arrow_up_n.data"
 #include "assets/inc/images/arrow_up_o.data"
 #include "assets/inc/images/arrow_up_p.data"
+#include "assets/inc/images/backspace.data"
 #include "assets/inc/images/check.data"
 #include "assets/inc/images/checked.data"
 #include "assets/inc/images/dialog_title.data"
@@ -65,71 +69,69 @@
 #include "assets/inc/images/empty.data"
 #include "assets/inc/images/radio_checked.data"
 #include "assets/inc/images/radio_unchecked.data"
+#include "assets/inc/images/shift.data"
+#include "assets/inc/images/shifton.data"
 #include "assets/inc/images/switch.data"
 #include "assets/inc/images/unchecked.data"
 #endif/*WITH_STB_IMAGE*/
 #ifdef WITH_VGCANVAS
 #endif/*WITH_VGCANVAS*/
-#if defined(WITH_STB_FONT) || defined(WITH_FT_FONT)
-#if defined(WITH_MINI_FONT)
-#else/*WITH_MINI_FONT*/
+#if defined(WITH_TRUETYPE_FONT)
 #include "assets/inc/fonts/default.res"
-#endif/*WITH_MINI_FONT*/
-#else/*WITH_STB_FONT or WITH_FT_FONT*/
-#endif/*WITH_STB_FONT or WITH_FT_FONT*/
+#else/*WITH_TRUETYPE_FONT*/
+#endif/*WITH_TRUETYPE_FONT*/
 #endif/*WITH_FS_RES*/
 
 ret_t assets_init(void) {
-  assets_manager_t* rm = assets_manager();
+  assets_manager_t* am = assets_manager();
 
 #ifdef WITH_FS_RES
-#if defined(WITH_MINI_FONT)
-  assets_manager_preload(rm, ASSET_TYPE_FONT, "default_mini");
-#else/*WITH_MINI_FONT*/
-  assets_manager_preload(rm, ASSET_TYPE_FONT, "default");
-#endif/*WITH_MINI_FONT*/
-  assets_manager_preload(rm, ASSET_TYPE_STYLE, "default");
+  assets_manager_preload(am, ASSET_TYPE_FONT, "default");
+  assets_manager_preload(am, ASSET_TYPE_STYLE, "default");
 #else
-  assets_manager_add(rm, image_arrow_down_n);
-  assets_manager_add(rm, image_arrow_down_o);
-  assets_manager_add(rm, image_arrow_down_p);
-  assets_manager_add(rm, image_arrow_left_n);
-  assets_manager_add(rm, image_arrow_left_o);
-  assets_manager_add(rm, image_arrow_left_p);
-  assets_manager_add(rm, image_arrow_right_n);
-  assets_manager_add(rm, image_arrow_right_o);
-  assets_manager_add(rm, image_arrow_right_p);
-  assets_manager_add(rm, image_arrow_up_n);
-  assets_manager_add(rm, image_arrow_up_o);
-  assets_manager_add(rm, image_arrow_up_p);
-  assets_manager_add(rm, image_check);
-  assets_manager_add(rm, image_checked);
-  assets_manager_add(rm, image_dialog_title);
-  assets_manager_add(rm, image_earth);
-  assets_manager_add(rm, image_edit_clear_n);
-  assets_manager_add(rm, image_edit_clear_o);
-  assets_manager_add(rm, image_edit_clear_p);
-  assets_manager_add(rm, image_empty);
-  assets_manager_add(rm, image_radio_checked);
-  assets_manager_add(rm, image_radio_unchecked);
-  assets_manager_add(rm, image_switch);
-  assets_manager_add(rm, image_unchecked);
-  assets_manager_add(rm, strings_en_US);
-  assets_manager_add(rm, strings_zh_CN);
-  assets_manager_add(rm, style_default);
-  assets_manager_add(rm, style_keyboard);
-  assets_manager_add(rm, style_main);
-  assets_manager_add(rm, style_window1);
-  assets_manager_add(rm, ui_kb_ascii);
-  assets_manager_add(rm, ui_kb_default);
-  assets_manager_add(rm, ui_kb_float);
-  assets_manager_add(rm, ui_kb_hex);
-  assets_manager_add(rm, ui_kb_int);
-  assets_manager_add(rm, ui_kb_phone);
-  assets_manager_add(rm, ui_kb_ufloat);
-  assets_manager_add(rm, ui_kb_uint);
-  assets_manager_add(rm, ui_main);
-  assets_manager_add(rm, ui_window1);
+  assets_manager_add(am, image_arrow_down_n);
+  assets_manager_add(am, image_arrow_down_o);
+  assets_manager_add(am, image_arrow_down_p);
+  assets_manager_add(am, image_arrow_left_n);
+  assets_manager_add(am, image_arrow_left_o);
+  assets_manager_add(am, image_arrow_left_p);
+  assets_manager_add(am, image_arrow_right_n);
+  assets_manager_add(am, image_arrow_right_o);
+  assets_manager_add(am, image_arrow_right_p);
+  assets_manager_add(am, image_arrow_up_n);
+  assets_manager_add(am, image_arrow_up_o);
+  assets_manager_add(am, image_arrow_up_p);
+  assets_manager_add(am, image_backspace);
+  assets_manager_add(am, image_check);
+  assets_manager_add(am, image_checked);
+  assets_manager_add(am, image_dialog_title);
+  assets_manager_add(am, image_earth);
+  assets_manager_add(am, image_edit_clear_n);
+  assets_manager_add(am, image_edit_clear_o);
+  assets_manager_add(am, image_edit_clear_p);
+  assets_manager_add(am, image_empty);
+  assets_manager_add(am, image_radio_checked);
+  assets_manager_add(am, image_radio_unchecked);
+  assets_manager_add(am, image_shift);
+  assets_manager_add(am, image_shifton);
+  assets_manager_add(am, image_switch);
+  assets_manager_add(am, image_unchecked);
+  assets_manager_add(am, strings_en_US);
+  assets_manager_add(am, strings_zh_CN);
+  assets_manager_add(am, style_default);
+  assets_manager_add(am, style_keyboard);
+  assets_manager_add(am, style_main);
+  assets_manager_add(am, style_window1);
+  assets_manager_add(am, ui_kb_ascii);
+  assets_manager_add(am, ui_kb_default);
+  assets_manager_add(am, ui_kb_float);
+  assets_manager_add(am, ui_kb_hex);
+  assets_manager_add(am, ui_kb_int);
+  assets_manager_add(am, ui_kb_phone);
+  assets_manager_add(am, ui_kb_ufloat);
+  assets_manager_add(am, ui_kb_uint);
+  assets_manager_add(am, ui_main);
+  assets_manager_add(am, ui_window1);
 #ifdef WITH_VGCANVAS
 #endif/*WITH_VGCANVAS*/
 #endif
