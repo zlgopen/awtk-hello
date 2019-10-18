c7820b2ea6d546da0919ccdfe66face0cf3e03b9
lixianjing <xianjimli@hotmail.com>
2019-10-18 03:21:50 -0700

    update assets

--- scripts/update_res.py
+++ scripts/update_res.py
@@ -13,7 +13,7 @@ if APP_ROOT.endswith('scripts'):
 
 os.chdir(APP_ROOT);
 AWTK_ROOT=common.joinPath(APP_ROOT, '../awtk');
-ASSETS_ROOT=common.joinPath(APP_ROOT, 'assets')
+ASSETS_ROOT=common.joinPath(APP_ROOT, 'assets/default')
 ASSET_C=common.joinPath(APP_ROOT, 'src/assets.c')
 
 common.init(AWTK_ROOT, ASSETS_ROOT, ASSET_C);
